730158284ebee72b193e951797e1fc2050e7ef07
steveroger241 <csjm2k20241@gmail.com>
2024-05-26 23:42:54 +0530

    first

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, './client/build')));
 
 const file = require('./jsondata.json');
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send(file);
 });
+
+app.use('*', (req, res) => {
+    req.sendFile(path.join(__dirname, './client/build/index.html'));
+})
 
 app.listen(8000);
